f4ed5a675a7e71d75992199d12acbdaea84e15ab
Louisa Isaeva$ <louisaisaeva2002@gmail.com>
2020-12-09 18:14:15 +0300

    corrected version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def output
     @arr = []
-    render xml: convert_to_xml(check_res(params[:txt]))
+    @arr = Result.check_res(params[:txt]) if check_params(params[:txt])
   end
 
   def serialize_db
@@ -16,30 +16,7 @@ class HomeController < ApplicationController
 
   protected
 
-  def convert_to_xml(arr)
-    Nokogiri::XML::Builder.new do
-      result do
-        arr&.each_with_index do |elem, i|
-          element do
-            index i+1
-            value elem 
-            square elem*elem
-          end
-        end
-      end
-    end
-  end
-
-  def check_res(req)
-    return Result.check_db(req) unless Result.check_db(req).nil?
-    if (check_params(req))
-      (req.to_i + 1).times { |i| @arr.append(i) if (i * i).to_s.reverse.to_i == (i * i) };
-      Result.insert_in_db(req, @arr)
-    end
-    @arr
-  end
-
   def check_params(str)
-    '!str.empty? && str.split.length == 1 && str.to_i.to_s == str ?'
+    !str.empty? && str.split.length == 1 && str.to_i.to_s == str
   end
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -2,8 +2,8 @@ class Result < ApplicationRecord
     validates :request, uniqueness: true
     
     def self.check_db(data)
-        return if find_by(request: data).nil?
-        JSON.parse(Result.find_by(request: data).response)
+        return if (res = find_by(request: data)).nil?
+        JSON.parse(res.response)
     end
 
     def self.insert_in_db(req, res)
@@ -12,8 +12,15 @@ class Result < ApplicationRecord
         record.save if record.valid?
     end
 
+    def self.check_res(req)
+        arr = []
+        return res = Result.check_db(req) unless res.nil?
+        (req.to_i + 1).times { |i| arr.append(i) if (i * i).to_s.reverse.to_i == (i * i) };
+        Result.insert_in_db(req, arr)
+        arr
+    end
+
     def self.parse_db
         Result.all.map { |rec| [rec.request, rec.response] }
     end
 end
-
